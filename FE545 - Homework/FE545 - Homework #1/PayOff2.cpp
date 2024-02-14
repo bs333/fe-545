9ba4e526ab9ca6929a06bc74aaea608c716d1aae
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-02-13 21:09:13 -0500

    Add all other necessary files.

---
+++ FE545 - Homework/FE545 - Homework #1/PayOff2.cpp
@@ -0,0 +1,35 @@
+//
+//  PayOff2.cpp
+//  Payoff_class_with_inheritance
+//
+//  Created by cheerzzh on 01/25/2022.
+//  Copyright (c) 2022 Steve Yang. All rights reserved.
+//
+
+#include "PayOff2.h"
+#include <algorithm> // for max() function
+
+PayOffCall::PayOffCall(BasePayOffParameters& Param_)
+{
+    Strike = 0;// Need to change
+}
+
+double PayOffCall::operator()(double Spot) const
+{
+    return std::max(Spot - Strike,0.0);
+
+}
+
+PayOffPut::PayOffPut(BasePayOffParameters& Param_)
+{
+    Strike = 0;// Need to change
+}
+
+double PayOffPut::operator()(double Spot) const
+{
+    return std::max(Strike - Spot,0.0);
+}
+
+
+
+
