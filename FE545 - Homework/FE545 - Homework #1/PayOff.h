c0c516b44aa16d05f60e1625f6c4af3bf9e1d88c
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-02-13 23:17:15 -0500

    Add to PayOff.h.

--- FE545 - Homework/FE545 - Homework #1/PayOff.h
+++ FE545 - Homework/FE545 - Homework #1/PayOff.h
@@ -1,63 +1,62 @@
-//
-//  PayOff.h
-//  Payoff_class_with_inheritance
-
 #ifndef __Payoff_class_with_inheritance__PayOff__
 #define __Payoff_class_with_inheritance__PayOff__
 
 #include <iostream>
+#include <algorithm> // Needed for std::max
 
-class PayOff //base class
-{
+class PayOff {
 public:
     PayOff(){};
-    // pure virtual function -> need not to be defined in base class
-    // must be defined in a inherited class
-    // the base class in incomplete 
-    virtual double operator()(double Spot) const = 0; //given spot,return payoff
+    virtual double operator()(double Spot) const = 0;
     virtual ~PayOff(){}
-    
-private:
-    
-}; 
-
-class BasePayOffParameters
-{
-public:
-    BasePayOffParameters(double Strike) : m_Strike(Strike) {}
-    virtual ~BasePayOffParameters() {};
-
-    double GetStrike() const { return m_Strike; }
-
-private:
-    double m_Strike;
 };
 
+class BasePayOffParameters {
+public:
+    BasePayOffParameters(){};
+    virtual ~BasePayOffParameters(){};
+};
 
-// class PayOffParameters //derived class
-
-class PayOffCall: public PayOff
-{
+class PayOffCall : public PayOff {
 public:
     PayOffCall(BasePayOffParameters& Param_);
-    virtual double operator()(double Spot) const; //overloading () to return payoff
+    virtual double operator()(double Spot) const;
     virtual ~PayOffCall(){}
 
 private:
     double Strike;
 };
 
-
-class PayOffPut: public PayOff
-{
+class PayOffPut : public PayOff {
 public:
     PayOffPut(BasePayOffParameters& Param_);
     virtual double operator()(double Spot) const;
     virtual ~PayOffPut(){}
-    
+
 private:
     double Strike;
-    
+};
+
+// New classes for Double Digital PayOff
+class DoubleDigitalPayOffParameters : public BasePayOffParameters {
+public:
+    DoubleDigitalPayOffParameters(double K1, double K2);
+    virtual ~DoubleDigitalPayOffParameters(){};
+    double GetLowerStrike() const;
+    double GetUpperStrike() const;
+
+private:
+    double K1, K2;
+};
+
+class PayOffDoubleDigital : public PayOff {
+public:
+    PayOffDoubleDigital(const DoubleDigitalPayOffParameters& Param_);
+    virtual double operator()(double Spot) const;
+    virtual ~PayOffDoubleDigital(){}
+
+private:
+    double K1, K2;
 };
 
 #endif /* defined(__Payoff_class_with_inheritance__PayOff__) */
