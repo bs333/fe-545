2caab7b2e7fe077d858d7d53a58c4e0a2287d37f
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-02-13 21:10:32 -0500

    Fix spacing in Random1.cpp

--- FE545 - Homework/FE545 - Homework #1/Random1.cpp
+++ FE545 - Homework/FE545 - Homework #1/Random1.cpp
@@ -22,7 +22,7 @@ double GetOneGaussianBySummation()
 {
 	double result = 0;
     
-	for(unsigned long j=0;j<12;j++)
+	for(unsigned long j = 0; j < 12; j++)
 		result += rand()/static_cast<double>(RAND_MAX);
     
 	result -= 6.0;
@@ -41,12 +41,14 @@ double GetOneGaussianByBoxMuller()
     
 	do
 	{
-		x = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
-		y = 2.0 * rand()/static_cast<double>(RAND_MAX)-1;
+		x = 2.0 * rand() / static_cast<double>(RAND_MAX) - 1;
+		y = 2.0 * rand() / static_cast<double>(RAND_MAX) - 1;
+
 		sizeSquared = x*x + y*y;
 	}
 	while (sizeSquared >= 1.0);
-	result = x*sqrt(-2*log(sizeSquared)/sizeSquared);
+
+	result = x * sqrt(-2 * log(sizeSquared) / sizeSquared);
     
 	return result;
 }
