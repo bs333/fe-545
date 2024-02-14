3a5b61f73a24a4af3b11df9f8274b9720c8e6382
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-02-13 23:11:06 -0500

    Write DoubleDigitalPayOffParameters class.

--- FE545 - Homework/FE545 - Homework #1/DoubleDigital.h
+++ FE545 - Homework/FE545 - Homework #1/DoubleDigital.h
@@ -2,3 +2,14 @@
 #ifndef __PayOffDoubleDigital__
 #define __PayOffDoubleDigital__
 #include "PayOff.h"
+
+class DoubleDigitalPayOffParameters : public BasePayOffParameters {
+public:
+    DoubleDigitalPayOffParameters(const double& K1, const double& K2);
+    virtual ~DoubleDigitalPayOffParameters() {};
+    double GetLowerStrike() const;
+    double GetUpperStrike() const;
+
+private:
+    double K1, K2;
+};
