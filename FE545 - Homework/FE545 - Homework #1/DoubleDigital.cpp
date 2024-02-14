edc586b6528a12b8e4734a529c667e18737a0cd2
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-02-13 23:13:39 -0500

    Create DoubleDigital.cpp and add constructor/destructors.

---
+++ FE545 - Homework/FE545 - Homework #1/DoubleDigital.cpp
@@ -0,0 +1,8 @@
+#include "DoubleDigital.h"
+
+// Constructor for DoubleDigitalPayOffParameters
+DoubleDigitalPayOffParameters::DoubleDigitalPayOffParameters(const double& K1, const double& K2)
+: BasePayOffParameters(), K1(K1), K2(K2) {}
+
+// Destructor for DoubleDigitalPayOffParameters
+DoubleDigitalPayOffParameters::~DoubleDigitalPayOffParameters() {}
