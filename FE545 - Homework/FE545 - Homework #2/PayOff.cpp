3d53fcda4b1987b6b2bc767845f08a098f679456
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 18:59:16 -0500

    Write comments for PayOffPut methods.

--- FE545 - Homework/FE545 - Homework #2/PayOff.cpp
+++ FE545 - Homework/FE545 - Homework #2/PayOff.cpp
@@ -19,9 +19,18 @@ double PayOffCall::operator()(const std::vector<double>& S) const
     return std::max(average - K, 0.0); // Max between 0 and (average spot price - K)
 }
 
+/* Constructor for PayOffPut, initializing the strike price (K). */
 PayOffPut::PayOffPut(const double& K_) : K(K_) {}
 
+/* Calculates the payoff for a put option.
+
+   The payoff is the maximum of zero or the difference between the
+   strike price and the average spot price.
+
+   Param S: Vector of spot prices.
+   Returns: Calculated payoff for the put option. */
 double PayOffPut::operator()(const std::vector<double>& S) const {
-    double average = std::exp(std::accumulate(S.begin(), S.end(), 0.0, [](double a, double b) { return a + std::log(b); }) / S.size());
-    return std::max(K - average, 0.0);
+    double average = std::exp(std::accumulate(S.begin(), S.end(), 0.0,
+                            [](double a, double b) { return a + std::log(b); }) / S.size());
+    return std::max(K - average, 0.0); // Max between 0 and (K - average spot price)
 }
