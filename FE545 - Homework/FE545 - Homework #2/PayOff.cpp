c0febf0a96619189d74117b44d0ea67daf11b474
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 18:55:38 -0500

    Write PayOffCall() operator method that computes payoff.

--- FE545 - Homework/FE545 - Homework #2/PayOff.cpp
+++ FE545 - Homework/FE545 - Homework #2/PayOff.cpp
@@ -2,3 +2,10 @@
 #include <cmath> // log, exp, sqrt
 
 PayOffCall::PayOffCall(const double& K_) : K(K_) {}
+
+
+double PayOffCall::operator()(const std::vector<double>& S) const
+{
+    double average = std::exp(std::accumulate(S.begin(), S.end(), 0.0, [](double a, double b) { return a + std::log(b); }) / S.size());
+    return std::max(average - K, 0.0);
+}
