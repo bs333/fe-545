3f25f2021b284e94c5f77e0d8f2e518de9fbdefc
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 18:58:30 -0500

    Write comments for PayOffCall methods.

--- FE545 - Homework/FE545 - Homework #2/PayOff.cpp
+++ FE545 - Homework/FE545 - Homework #2/PayOff.cpp
@@ -2,12 +2,21 @@
 #include <cmath> // For std::exp
 #include <numeric> // For std::accumulate
 
+/* Constructor for PayOffCall, initializing the strike price (K). */
 PayOffCall::PayOffCall(const double& K_) : K(K_) {}
 
+/* Calculates the payoff for a call option.
+
+   The payoff is the maximum of zero or the difference between the
+   average spot price and the strike price.
+
+   Param S: Vector of spot prices.
+   Returns: Calculated payoff for the call option. */
 double PayOffCall::operator()(const std::vector<double>& S) const
 {
-    double average = std::exp(std::accumulate(S.begin(), S.end(), 0.0, [](double a, double b) { return a + std::log(b); }) / S.size());
-    return std::max(average - K, 0.0);
+    double average = std::exp(std::accumulate(S.begin(), S.end(), 0.0,
+                            [](double a, double b) { return a + std::log(b); }) / S.size());
+    return std::max(average - K, 0.0); // Max between 0 and (average spot price - K)
 }
 
 PayOffPut::PayOffPut(const double& K_) : K(K_) {}
