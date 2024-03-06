227302b75c30a61c23d1a71dbe094e2033ad44c9
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 18:56:07 -0500

    Write PayOffPut constructor.

--- FE545 - Homework/FE545 - Homework #2/PayOff.cpp
+++ FE545 - Homework/FE545 - Homework #2/PayOff.cpp
@@ -3,9 +3,10 @@
 
 PayOffCall::PayOffCall(const double& K_) : K(K_) {}
 
-
 double PayOffCall::operator()(const std::vector<double>& S) const
 {
     double average = std::exp(std::accumulate(S.begin(), S.end(), 0.0, [](double a, double b) { return a + std::log(b); }) / S.size());
     return std::max(average - K, 0.0);
 }
+
+PayOffPut::PayOffPut(const double& K_) : K(K_) {}
