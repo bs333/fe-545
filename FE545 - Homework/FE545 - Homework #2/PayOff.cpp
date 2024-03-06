37880d0530d2ac1e027f4db5eb83258d26f45499
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 18:56:33 -0500

    Write PayOffPut operator() method that computes payoff.

--- FE545 - Homework/FE545 - Homework #2/PayOff.cpp
+++ FE545 - Homework/FE545 - Homework #2/PayOff.cpp
@@ -10,3 +10,8 @@ double PayOffCall::operator()(const std::vector<double>& S) const
 }
 
 PayOffPut::PayOffPut(const double& K_) : K(K_) {}
+
+double PayOffPut::operator()(const std::vector<double>& S) const {
+    double average = std::exp(std::accumulate(S.begin(), S.end(), 0.0, [](double a, double b) { return a + std::log(b); }) / S.size());
+    return std::max(K - average, 0.0);
+}
