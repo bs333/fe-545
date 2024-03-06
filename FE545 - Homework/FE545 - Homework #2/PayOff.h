5a08fa3670232c0f4df583ef70d5307d4163a0bb
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 17:39:56 -0500

    Write the comments for PayOffCall inerhited class.

--- FE545 - Homework/FE545 - Homework #2/PayOff.h
+++ FE545 - Homework/FE545 - Homework #2/PayOff.h
@@ -16,21 +16,34 @@ class PayOff
         /* Overloaded operator() to make this class a callable object (function object).
 
         Must be overridden in derived classes to compute the option payoff.
-        
+
         Param S: The spot price(s) of the underlying asset.
         Returns: The calculated payoff. */
         virtual double operator()(const std::vector<double>& S) const = 0;
 };
 
+/* PayOffCall: Derived class to calculate the payoff of a call option. */
 class PayOffCall : public PayOff
 {
     public:
+        /* Constructor to initialize the strike price of the call option.
+        
+        Param K_: The strike price. */
         PayOffCall(const double& K_);
+
+        /* Destructor. */
         virtual ~PayOffCall() {}
+        
+        /* Calculates the payoff of the call option based on spot price(s).
+
+        Overrides the pure virtual function of the base class.
+
+        Param S: The spot price(s) of the underlying asset.
+        Returns: The calculated call option payoff. */
         virtual double operator()(const std::vector<double>& S) const override;
 
     private:
-        double K; // Strike price
+        double K; // Strike price of the option
 };
 
 class PayOffPut : public PayOff
