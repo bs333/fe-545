7580ab25edd7951421538127cf5209fcca7c649e
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 17:38:44 -0500

    Write comments for PayOff base class.

--- FE545 - Homework/FE545 - Homework #2/PayOff.h
+++ FE545 - Homework/FE545 - Homework #2/PayOff.h
@@ -1,13 +1,24 @@
 #ifndef __PAY_OFF__
 #define __PAY_OFF__
 
-#include <algorithm> // For std::max
+#include <algorithm> // For std::max, used in payoff calculations.
 
+/* PayOff: Abstract base class for option payoff calculations.
+
+   Implements the Function Object design pattern to calculate
+   the payoff for given spot price(s) of the underlying asset. */
 class PayOff
 {
     public:
-        PayOff() {}
-        virtual ~PayOff() {}
+        PayOff() {} // Default constructor
+        virtual ~PayOff() {} // Virtual destructor for proper cleanup of derived classes
+        
+        /* Overloaded operator() to make this class a callable object (function object).
+
+        Must be overridden in derived classes to compute the option payoff.
+        
+        Param S: The spot price(s) of the underlying asset.
+        Returns: The calculated payoff. */
         virtual double operator()(const std::vector<double>& S) const = 0;
 };
 
