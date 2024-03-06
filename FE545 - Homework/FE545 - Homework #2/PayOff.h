5bc31b158e8d4fba994fea6e1beffef02a8fac8c
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 16:54:24 -0500

    Write PayOff class.

--- FE545 - Homework/FE545 - Homework #2/PayOff.h
+++ FE545 - Homework/FE545 - Homework #2/PayOff.h
@@ -3,4 +3,12 @@
 
 #include <algorithm> // For std::max
 
+class PayOff
+{
+    public:
+        PayOff() {}
+        virtual ~PayOff() {}
+        virtual double operator()(const std::vector<double>& S) const = 0;
+};
+
 #endif
