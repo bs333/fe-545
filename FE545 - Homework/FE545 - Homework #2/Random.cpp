c0c1960e522a4589de24dd428ea14505707a4514
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 16:13:37 -0500

    Write do-while loop.

--- FE545 - Homework/FE545 - Homework #2/Random.cpp
+++ FE545 - Homework/FE545 - Homework #2/Random.cpp
@@ -7,4 +7,13 @@ double GetOneGaussianByBoxMuller()
     double x, y;
 
     double sizeSquared, result;
+
+    do
+    {
+        x = 2.0 * rand() / static_cast<double>(RAND_MAX) - 1;
+        y = 2.0 * rand() / static_cast<double>(RAND_MAX) - 1;
+
+        sizeSquared = x * x + y * y;
+    } while (sizeSquared >= 1.0 || sizeSquared == 0.0);
+    
 }
