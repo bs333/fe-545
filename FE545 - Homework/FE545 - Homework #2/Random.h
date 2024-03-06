c6a03b283701e8d289f2930d41ada50652e637d1
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 16:09:40 -0500

    Write methods to be defined in Random.cpp and finish Random.h header file.

--- FE545 - Homework/FE545 - Homework #2/Random.h
+++ FE545 - Homework/FE545 - Homework #2/Random.h
@@ -1,4 +1,14 @@
 #ifndef __Option_Class__Random__
 #define __Option_Class__Random__
 
-#endif
+// Generates a random Gaussian number.
+double GetOneGaussianByBoxMuller();
+
+// Generates a Geometric Brownian Motion spot price path.
+void GetGBMSpotPricePath(std::vector<double>& spotPrices,
+                         const double& r,
+                         const double& v,
+                         const double& T,
+                         unsigned long NumberOfSteps);
+
+#endif /* defined(__Option_Class__Random__) */
