90e66b00cc68693adf1db0f25bc55be2d0939ad7
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-07 07:08:47 -0500

    Write function definition for SimpleMonteCarlo3.

--- FE545 - Homework/FE545 - Homework #2/SimpleMC.cpp
+++ FE545 - Homework/FE545 - Homework #2/SimpleMC.cpp
@@ -1,3 +1,13 @@
 #include "SimpleMC.h"
 #include "Random.h"
 #include <vector>
+
+double SimpleMonteCarlo3(const AsianOption& TheOption,
+                         double Spot,
+                         double Vol,
+                         double r,
+                         unsigned long NumberOfPaths,
+                         unsigned long NumberOfSteps) 
+{
+    
+}
