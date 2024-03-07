350576c481e99de6dc84eca3dfe90e28f8f6b1f5
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 19:32:06 -0500

    Add AsianOption constructor.

--- FE545 - Homework/FE545 - Homework #2/AsianOption.cpp
+++ FE545 - Homework/FE545 - Homework #2/AsianOption.cpp
@@ -1,3 +1,5 @@
 #include "AsianOption.h"
 #include <numeric> // For std::accumulate
 #include <cmath>   // For std::exp and std::log
+
+AsianOption::AsianOption(PayOff* _pay_off) : pay_off(_pay_off) {}
