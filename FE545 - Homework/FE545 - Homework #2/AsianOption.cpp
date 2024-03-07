157ffc4b2f46a38896e0d3afbc30cd49bc4c59dd
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 19:34:15 -0500

    Add comments for AsianOptionGeometric constructor.

--- FE545 - Homework/FE545 - Homework #2/AsianOption.cpp
+++ FE545 - Homework/FE545 - Homework #2/AsianOption.cpp
@@ -7,6 +7,9 @@
    Param _pay_off: Pointer to a PayOff object which will be used to calculate payoffs. */
 AsianOption::AsianOption(PayOff* _pay_off) : pay_off(_pay_off) {}
 
+/* Constructor for the AsianOptionGeometric class. Calls the base class constructor to initialize pay_off.
+
+   Param _pay_off: Pointer to a PayOff object for payoff calculation logic. */
 AsianOptionGeometric::AsianOptionGeometric(PayOff* _pay_off) : AsianOption(_pay_off) {}
 
 double AsianOptionGeometric::OptionPayOff(const std::vector<double>& spot_prices) const 
