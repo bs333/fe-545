9f13758e4027d0a647dd6da3ad35d6f2bd9ccd92
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-07 07:21:29 -0500

    Define and initialize variables.

--- FE545 - Homework/FE545 - Homework #2/AsianOptionMain.cpp
+++ FE545 - Homework/FE545 - Homework #2/AsianOptionMain.cpp
@@ -4,5 +4,15 @@
 
 int main()
 {
+    double Expiry = 1.0;
+    double Strike = 50;
+    double Spot = 50;
+    double Vol = 0.30;
+    double r = 0.05;
     
+    unsigned long NumberOfPaths = 1000;
+    unsigned long NumberOfSteps = 250;
+
+
+
 }
