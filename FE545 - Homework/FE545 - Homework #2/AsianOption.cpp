ad789c2abe2d8d15b48eae8618847857ba002229
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 19:32:26 -0500

    Add AsianOptionGeometric constructor.

--- FE545 - Homework/FE545 - Homework #2/AsianOption.cpp
+++ FE545 - Homework/FE545 - Homework #2/AsianOption.cpp
@@ -3,3 +3,5 @@
 #include <cmath>   // For std::exp and std::log
 
 AsianOption::AsianOption(PayOff* _pay_off) : pay_off(_pay_off) {}
+
+AsianOptionGeometric::AsianOptionGeometric(PayOff* _pay_off) : AsianOption(_pay_off) {}
