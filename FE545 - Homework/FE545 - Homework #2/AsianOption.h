4c2966bc904fcb890dea0eb397239b5dfdce0177
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-06 19:25:50 -0500

    Write AsianOption class.

--- FE545 - Homework/FE545 - Homework #2/AsianOption.h
+++ FE545 - Homework/FE545 - Homework #2/AsianOption.h
@@ -4,5 +4,15 @@
 #include <vector>
 #include "PayOff.h"
 
+class AsianOption
+{
+    public:
+        AsianOption(PayOff* _pay_off);
+        virtual ~AsianOption() {}
+        virtual double OptionPayOff(const std::vector<double>& spot_prices) const = 0;
+
+    protected:
+        PayOff* pay_off; // Pay-off class (in this instance call or put)
+};
 
 #endif
