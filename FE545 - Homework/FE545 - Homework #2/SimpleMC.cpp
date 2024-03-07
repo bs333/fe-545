21da4cf4163a75b4f19c93f89310bdeb76779c6f
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-03-07 07:14:43 -0500

    Define and initialize variables.

--- FE545 - Homework/FE545 - Homework #2/SimpleMC.cpp
+++ FE545 - Homework/FE545 - Homework #2/SimpleMC.cpp
@@ -9,5 +9,13 @@ double SimpleMonteCarlo3(const AsianOption& TheOption,
                          unsigned long NumberOfPaths,
                          unsigned long NumberOfSteps) 
 {
-    
+    double expiry = 1.0;
+
+    double variance = Vol * Vol * expiry;
+    double rootVariance = sqrt(variance);
+    double itoCorrection = -0.5 * variance;
+
+    double movedSpot = Spot * exp(r * expiry + itoCorrection);
+    double thisSpot;
+    double runningSum = 0;
 }
