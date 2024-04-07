5ee663b7749ec67e9ad90fb1dd5167b80965b773
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-04-06 23:21:42 -0400

    Write TrinomialTree.h.

--- FE545 - Homework/FE545 - Homework #3/fe545-hw3-files/TrinomialTree.h
+++ FE545 - Homework/FE545 - Homework #3/fe545-hw3-files/TrinomialTree.h
@@ -2,3 +2,35 @@
 
 #ifndef TRINOMIALTREE_H
 #define TRINOMIALTREE_H
+
+#include "TreeProducts.h"
+#include "Parameters.h"
+#include "Arrays.h"
+#include <vector>
+
+class SimpleTrinomialTree {
+public:
+    SimpleTrinomialTree(double Spot_,
+                        const Parameters& r_,
+                        double Volatility_,
+                        unsigned long Steps_,
+                        double Time_);
+
+    double GetThePrice(const TreeProduct& TheProduct);
+
+protected:
+    void BuildTree();
+
+private:
+    double Spot;
+    Parameters r;
+    double Volatility;
+    unsigned long Steps;
+    double Time;
+    bool TreeBuilt;
+
+    std::vector<std::vector<std::vector<double>>> TheTree;
+    MJArray Discounts;
+};
+
+#endif /* TRINOMIALTREE_H */
