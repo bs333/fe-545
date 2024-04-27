1d8ada419f4cbae20ef5a49894cba5431b21c37d
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-04-26 23:42:45 -0400

    main.cpp

--- FE545 - Homework/FE545 - Homework #4/fe545-hw4-cpp/main.cpp
+++ FE545 - Homework/FE545 - Homework #4/fe545-hw4-cpp/main.cpp
@@ -1,69 +1,41 @@
-//
-//  main.cpp
-//  ch9-solver-option-vol
-//
-//  Created by Steve Yang on 11/2/21.
-//
-
-/* Needs
- BlackScholesFormulas.cpp
- BSCallClass.cpp
- Normals.cpp
- BSCallTwo.cpp
- */
-
 #include <iostream>
-#include "Bisection.h"
 #include "BSCallClass.h"
 #include "BlackScholesFormulas.h"
 #include "NewtonRaphso.h"
 #include "BSCallTwo.h"
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
     double Expiry = 1.0;
-    double Strike = 50;
-    double Spot = 50;
+    double Strike = 50.0;
+    double Spot = 50.0;
     double r = 0.05;
-    double d = 0.08;
+    double d = 0.08; // This is the continuous dividend yield
     double Price = 4.23;
-        
-    double low,high;
     
-    cout << "\nlower volatility guess\n";
-    cin >> low;
-    cout << "\nhigh volatility guess\n";
-    cin >> high;
-    
-    double start;
-    cout << "\nstart volatility guess\n";
+    double start; // This will be the starting guess for the implied volatility
+    cout << "Enter start volatility guess: ";
     cin >> start;
     
     double tolerance;
-    
-    cout << "\nTolerance for volatility\n";
+    cout << "Enter Tolerance for Newton-Raphson: ";
     cin >> tolerance;
     
-    // function object for a vanilla call with all given parameters
-    BSCall theCall(r,d,Expiry,Spot,Strike);
+    // Create function object for a vanilla call with all given parameters
+    BSCallTwo theCall(r, d, Expiry, Spot, Strike);
     
-    // find volatility using bisection method (Bisection object function)
-    double vol = 0.0;
+    // Use Newton-Raphson method to find the implied volatility
+    double vol = NewtonRaphson<BSCallTwo, &BSCallTwo::Price, &BSCallTwo::Vega>(Price, start, tolerance, theCall);
 
-    cout << "\nUse bisection method:\n";
-    cout << "\nimplied vol is:  " << vol << endl;
-
-    // check the price using BlackSholesCall with the implied vol
-    double PriceTwo = 0.0;
+    cout << "\nUsing Newton-Raphson method:\n";
+    cout << "Implied vol is: " << vol << endl;
     
-    cout << " \nOption price by implied volatility is: " << PriceTwo << "\n";
-
-    double tmp;
-    cin >> tmp;
+    // Check the price using BlackScholesCall with the implied vol
+    double PriceTwo = BlackScholesCall(Spot, Strike, r, d, vol, Expiry);
+    
+    cout << "Option price by implied volatility is: " << PriceTwo << endl;
     
     return 0;
 }
-
